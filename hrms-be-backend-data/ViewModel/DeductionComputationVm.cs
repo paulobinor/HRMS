e53b6947a79614ade51351af540ccde4f56cf845
Peter.Omolayo <Peter.Omolayo@xpresspayments.com>
2023-09-28 18:47:29 +0100

    Sync

--- hrms-be-backend-data/ViewModel/DeductionComputationVm.cs
+++ hrms-be-backend-data/ViewModel/DeductionComputationVm.cs
@@ -3,9 +3,9 @@
     public class DeductionComputationVm
     {
         public long EarningsItemId { get; set; }
+        public string EarningItemName { get; set; }
         public string DeductionName { get; set; }
-        public long DeductionId { get; set; }
-        public string EarningsName { get; set; }
+        public long DeductionId { get; set; }      
         public long CreatedByUserId { get; set; }
         public DateTime DateCreated { get; set; }
     }
