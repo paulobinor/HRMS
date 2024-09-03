0fe962b9de6a27e81e767cb395878b11b9e32ee0
ifeanyi.obinor <ifeanyi.obinor@xpresspayments.com>
2024-09-03 23:41:02 +0100

    updates

--- hrms-be-backend-api/Controllers/ResignationController.cs
+++ hrms-be-backend-api/Controllers/ResignationController.cs
@@ -115,7 +115,7 @@ namespace hrms_be_backend_api.ExitModuleController.Controller
 
         [HttpGet]
         [Route("GetResignationByCompanyID/{companyId}")]
-      //  [Authorize]
+        [Authorize]
         public async Task<IActionResult> GetResignationByCompanyID(long companyId, [FromQuery] PaginationFilter filter, DateTime? startDate, DateTime? endDate)
         {
            
